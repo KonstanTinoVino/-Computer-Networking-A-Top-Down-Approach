2ebb04f7ad48bd45171d493a6c1b4490cd4bbe82
Konstantin <konstantn.petrov.ivanov@everis.com>
2021-08-11 17:11:41 +0200

    Some bug fixes

--- sample/Chapter2/Exercises/AssignmentUDPPinger/UDPClient.py
+++ sample/Chapter2/Exercises/AssignmentUDPPinger/UDPClient.py
@@ -1,8 +1,10 @@
 from socket import *
 from datetime import datetime
+import time
 
 
 RTT_times = []
+lost = 0
 
 # Creating Object Addresses
 server_address = ('localhost', 12000)
@@ -17,12 +19,20 @@ clientSocket.settimeout(1)
 for i in range(10):
     message = "message: " + str(i)
 
-    start_time = datetime.now()
+    start_time = time.perf_counter()
     clientSocket.sendto(message.encode(), server_address)
     try:
         modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-        end_time = datetime.now()
+        end_time = time.perf_counter()
         RTT = end_time - start_time
+        RTT_times.append(RTT)
         print("Ping " + modifiedMessage.decode() + " " + str(RTT))
     except:
+        lost += 1
         print("Request timed out")
+
+print("\n")
+print("Maximum RTT = " + str(max(RTT_times)))
+print("Minimum RTT = " + str(min(RTT_times)))
+print("Average RTT = " + str(sum(RTT_times)/float(len(RTT_times))))
+print("Packet Loss Percentage = " + str(float(lost)/10 * 100))
